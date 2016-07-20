c5b197a4143c0d272dc5bc2dbef89129330f2641
Brandon <brandon.culver@gatech.edu>
2016-07-20 10:30:11 -0400

    re-rerouted page files

---
+++ assets/js/dash.js
@@ -0,0 +1,29 @@
+angular.module('myApp', ['ngAnimate', 'ui.bootstrap']);
+angular.module('myApp').controller('AccordionDemoCtrl', function ($scope) {
+  $scope.oneAtATime = false;
+
+  $scope.groups = [
+    {
+      title: 'Dynamic Group Header - 1',
+      content: 'Dynamic Group Body - 1'
+    },
+    {
+      title: 'Dynamic Group Header - 2',
+      content: 'Dynamic Group Body - 2'
+    }
+  ];
+
+  $scope.items = ['Item 1', 'Item 2', 'Item 3'];
+  $scope.opened = true;
+
+  $scope.addItem = function() {
+    var newItemNo = $scope.items.length + 1;
+    $scope.items.push('Item ' + newItemNo);
+  };
+
+  $scope.status = {
+    isCustomHeaderOpen: false,
+    isFirstOpen: true,
+    isFirstDisabled: false
+  };
+});
